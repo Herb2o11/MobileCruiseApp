057941e69fe8d10379defa7ca2fb2292c7121d7d
Botelho Dias <botelhodiash@student.douglascollege.ca>
2019-11-04 11:43:31 -0800

    Created the Depart Page, dut don't program the functions yet

---
+++ app/src/main/java/com/example/test/Depart_Page.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Depart_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_depart__page);
+    }
+}

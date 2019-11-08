ef73178e738b1a16de414b55fee68ed07c7700de
Administrator <admin@Administrators-MacBook-Pro.local>
2019-11-08 14:37:23 -0800

    Created Welcome Page, Login Page 10%, Registration Page 3% done

---
+++ app/src/main/java/com/example/test/Register.java
@@ -0,0 +1,29 @@
+package com.example.test;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Register extends AppCompatActivity {
+    TextInputLayout tiEmail, tiPassword, tiConfirmPassword, tiName, tiPhone, tiAddress, tiEmailRecommend;
+    EditText etEmail, etPassword, etConfirmPassword, etName, etPhone, etAddress, etEmailRecommend;
+    Button btnSubmit;
+
+    Boolean isEdit;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Intent i = getIntent();
+        if (i != null)
+            isEdit = i.getBooleanExtra("isEdit", false);
+        //If is edit change the theme before the view is created
+        if (isEdit)
+            setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/main/java/com/example/test/Welcome.java
@@ -0,0 +1,39 @@
+package com.example.test;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcome extends AppCompatActivity implements View.OnClickListener{
+    Button btnLogin;
+    Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        btnLogin=(Button)findViewById(R.id.btnLogin);
+        btnRegister=(Button)findViewById(R.id.btnSignUp);
+        btnRegister.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnLogin:
+                Intent iLogin=new Intent(this, Login.class);
+                startActivity(iLogin);
+                break;
+            case R.id.btnSignUp:
+                Intent iRegister= new Intent(this, Register.class);
+                startActivity(iRegister);
+                break;
+                default:
+                    break;
+        }
+    }
+}
